6747989290ee2a768a03d1ea9ea01f8f18eadfaa
alsoraimi <alsoraimi46@gmail.com>
2017-04-26 11:18:11 -0400

    initial commit, made seperate classes with functionality. no validation yet

---
+++ Lab15/CountriesTextFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab15
+{
+    class CountriesTextFile
+    {
+        public static void WriteToFile(string input)
+        {
+            StreamWriter sw = new StreamWriter("../../Countries.txt", true);  //the ..//..// tells it to go two levels up
+
+
+            sw.WriteLine(input);
+
+            sw.Close();
+        }
+
+        public static string ReadFromFile()
+        {
+            StreamReader rd = new StreamReader("../../Countries.txt");
+
+            string FileContent = rd.ReadToEnd();  //read to end reads the whole file
+
+            Console.WriteLine(FileContent);
+            rd.Close();
+            return FileContent;
+        }
+
+    }
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool AskToContinue = false;
+
+            do
+            {
+                string input;
+
+                Console.WriteLine("Please choose an option 1-Add a country,  2-See list of countries,  or 3-Exit");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.WriteLine("Please input a country");
+                    string choice = Console.ReadLine();
+                    CountriesTextFile.WriteToFile(choice);
+                    Console.WriteLine("Your country has been saved!");
+
+                }
+                else if (input == "2")
+                {
+                    CountriesTextFile.ReadFromFile();
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Thanks for trying!");
+                    break;
+
+                }
+                string continueinput;
+                Console.WriteLine("Would you like to try again?");
+                continueinput = Console.ReadLine().ToUpper();
+                if (continueinput == "Y")
+                {
+                    AskToContinue = true;
+                }
+                else
+                {
+                    Console.WriteLine("thanks for trying!");
+                    return;
+                }
+            } while (AskToContinue);
+
+        }
+    }
+}
